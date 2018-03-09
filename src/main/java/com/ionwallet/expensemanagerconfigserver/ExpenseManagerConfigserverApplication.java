7086739ee5c4cd6ff91fc953678c7c6b81d9377a
Sharath Mekottil <sharath.m@tavant.com>
2018-03-09 13:13:27 +0530

    first commit

---
+++ src/main/java/com/ionwallet/expensemanagerconfigserver/ExpenseManagerConfigserverApplication.java
@@ -0,0 +1,12 @@
+package com.ionwallet.expensemanagerconfigserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExpenseManagerConfigserverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExpenseManagerConfigserverApplication.class, args);
+	}
+}
